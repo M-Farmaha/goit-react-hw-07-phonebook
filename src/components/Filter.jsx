878752fea55fd9641ee51cc0195d20cad47df8bf
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-06-19 00:02:22 +0300

    add debounce on filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,18 +3,30 @@ import { ContactFeature, Input } from './styled';
 import { useSelector, useDispatch } from 'react-redux';
 import { setFilter } from 'redux/slice';
 import { getFilter } from 'redux/selectors';
+import { useEffect, useState } from 'react';
 
 export const Filter = () => {
   const filter = useSelector(getFilter);
   const dispatch = useDispatch();
+  const [inputValue, setInputValue] = useState(filter);
+
+  useEffect(() => {
+    const debounce = setTimeout(() => {
+      dispatch(setFilter(inputValue));
+    }, 300);
+
+    return () => {
+      clearTimeout(debounce);
+    };
+  }, [dispatch, inputValue]);
 
   return (
     <>
       <ContactFeature>Find contacts by name</ContactFeature>
       <Input
         type="text"
-        value={filter}
-        onChange={e => dispatch(setFilter(e.target.value))}
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
       />
     </>
   );
